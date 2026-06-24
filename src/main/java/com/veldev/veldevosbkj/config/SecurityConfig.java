13401b5ec9eabc19c00fa7941479354f2f52bb71
Leonardo <leonardomott.git@gmail.com>
2026-06-24 19:38:58 -0300

    fix: add Vercel URL to CORS

--- src/main/java/com/veldev/veldevosbkj/config/SecurityConfig.java
+++ src/main/java/com/veldev/veldevosbkj/config/SecurityConfig.java
@@ -80,7 +80,7 @@ public class SecurityConfig {
 
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:3000",
-                "http://localhost:5173"
+                "https://veldev-os.vercel.app"
         ));
 
         configuration.setAllowedMethods(List.of(
